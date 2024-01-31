1012542f31db4c59e4b8f9a400befbc7b0f99a5f
Blart Nicolas <nicolas_blart@ens.univ-artois.fr>
2024-01-31 20:05:21 +0100

    :sparkles: Ajout de la HomePage

---
+++ src/components/home/Actus.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import annonces from '../../annonces.json'
+
+const Actus = () => {
+    return (
+        <div className={'actus'}>
+            <div className={'container'}>
+                {annonces.annonces.map((annonce, index) => (
+                    <div key={index} className={'annonce'} style={{backgroundImage: `url(${annonce.imageFond})`}}>
+                        <h2>{annonce.titre}</h2>
+                        <p className={'date'}>Date: {annonce.date}</p>
+                        <p className={'desc'}>{annonce.description}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Actus;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import '../styles/pages/homepage.scss'
+import NavBar from "../components/global/NavBar";
+import Actus from "../components/home/Actus";
 
 const HomePage = () => {
     return (
-        <div>
-            
+        <div className={"home"}>
+            <NavBar/>
+            <div className={'title'}>
+                <h1>DISCORD.GG/RANKEDTOWER</h1>
+                <h3>LE SOMMET DE LA COMPÉTITION !</h3>
+            </div>
+            <div className={'sep'}></div>
+            <Actus/>
+            <div className={'sep2'}></div>
         </div>
     );
 };
