c1f35218e8d2c17240a125962b792b9e4378680c
Blart Nicolas <nicolas_blart@ens.univ-artois.fr>
2024-01-31 22:28:48 +0100

    :deploy: fix images for deploy

--- src/components/global/NavBar.tsx
+++ src/components/global/NavBar.tsx
@@ -23,123 +23,123 @@ const NavBar = () => {
     return (
         <div className={'nav'}>
             <div className={'logo'}>
-                <img src={'rankedtower-web/img/Logo.png'} alt={'Logo RT'}/>
+                <img src={'/rankedtower-web/img/Logo.png'} alt={'Logo RT'}/>
             </div>
             <div className={'navlink'}>
                 <div className={'link'}>
-                    <img src={'rankedtower-web/icons/home.svg'} alt={'home icon'}/>
+                    <img src={'/rankedtower-web/icons/home.svg'} alt={'home icon'}/>
                     <p>Accueil</p>
                 </div>
                 <div className={'link'}>
-                    <img src={'rankedtower-web/icons/leaderboard.svg'} alt={'medal icon'}/>
+                    <img src={'/rankedtower-web/icons/leaderboard.svg'} alt={'medal icon'}/>
                     <p>Leaderboard</p>
                 </div>
                 <div className={'link'} onClick={togglePalmaresMenu}>
-                    <img src={'rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
+                    <img src={'/rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
                     <p>Palmarès</p>
                     {!isPalmaresMenuVisible && (
-                        <img src={'rankedtower-web/icons/polygon.svg'} alt={'polygon icon'} className={'polygon'}/>
+                        <img src={'/rankedtower-web/icons/polygon.svg'} alt={'polygon icon'} className={'polygon'}/>
                     )}
                     {isPalmaresMenuVisible && (
-                        <img src={'rankedtower-web/icons/polygon.svg'} alt={'polygon icon'} style={{transform: 'rotate(180deg)'}}/>
+                        <img src={'/rankedtower-web/icons/polygon.svg'} alt={'polygon icon'} style={{transform: 'rotate(180deg)'}}/>
                     )}
                 </div>
                 {isPalmaresMenuVisible && (
                     <div className={'palmares-menu'}>
                         <div className={'link'}>
-                            <img src={'rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
+                            <img src={'/rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
                             <p>All Stars 1</p>
                         </div>
                         <div className={'link'}>
-                            <img src={'rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
+                            <img src={'/rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
                             <p>All Stars 4</p>
                         </div>
                         <div className={'link'}>
-                            <img src={'rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
+                            <img src={'/rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
                             <p>All Stars 3</p>
                         </div>
                         <div className={'link'}>
-                            <img src={'rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
+                            <img src={'/rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
                             <p>All Stars 4</p>
                         </div>
                     </div>
                 )}
                 <div className={'link'}>
-                    <img src={'rankedtower-web/icons/support.svg'} alt={'support icon'}/>
+                    <img src={'/rankedtower-web/icons/support.svg'} alt={'support icon'}/>
                     <p>Support</p>
                 </div>
                 <div className={'link'}>
-                    <img src={'rankedtower-web/icons/staff.svg'} alt={'shield icon'}/>
+                    <img src={'/rankedtower-web/icons/staff.svg'} alt={'shield icon'}/>
                     <p>Staffs</p>
                 </div>
                 <div className={'link'} onClick={openMapsLink}>
-                    <img src={'rankedtower-web/icons/map.svg'} alt={'pin icon'}/>
+                    <img src={'/rankedtower-web/icons/map.svg'} alt={'pin icon'}/>
                     <p>Maps</p>
                 </div>
                 <div className={'link'}>
-                    <img src={'rankedtower-web/icons/rules.svg'} alt={'list icon'}/>
+                    <img src={'/rankedtower-web/icons/rules.svg'} alt={'list icon'}/>
                     <p>Règlement</p>
                 </div>
             </div>
             <div className={'burger'} onClick={toggleMenu}>
-                <img src={'rankedtower-web/icons/burger-menu.svg'} alt={'burger menu icon'}/>
+                <img src={'/rankedtower-web/icons/burger-menu.svg'} alt={'burger menu icon'}/>
             </div>
 
             {isMenuVisible && (
                 <div className={'menu'}>
                     <div className={'link'}>
-                        <img src={'rankedtower-web/icons/home.svg'} alt={'home icon'}/>
+                        <img src={'/rankedtower-web/icons/home.svg'} alt={'home icon'}/>
                         <p>Accueil</p>
                     </div>
                     <div className={'link'}>
-                        <img src={'rankedtower-web/icons/leaderboard.svg'} alt={'medal icon'}/>
+                        <img src={'/rankedtower-web/icons/leaderboard.svg'} alt={'medal icon'}/>
                         <p>Leaderboard</p>
                     </div>
                     <div className={'link'} onClick={togglePalmaresMenu}>
-                        <img src={'rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
+                        <img src={'/rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
                         <p>Palmarès</p>
                         {!isPalmaresMenuVisible && (
-                            <img src={'rankedtower-web/icons/polygon.svg'} alt={'polygon icon'} className={'polygon'}/>
+                            <img src={'/rankedtower-web/icons/polygon.svg'} alt={'polygon icon'} className={'polygon'}/>
                         )}
                         {isPalmaresMenuVisible && (
-                            <img src={'rankedtower-web/icons/polygon.svg'} alt={'polygon icon'} style={{transform: 'rotate(90deg)'}}/>
+                            <img src={'/rankedtower-web/icons/polygon.svg'} alt={'polygon icon'} style={{transform: 'rotate(90deg)'}}/>
                         )}
                     </div>
                     {isPalmaresMenuVisible && (
 
                         <div className={'palmares-menu'}>
                             <div className={'link'}>
-                                <img src={'rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
+                                <img src={'/rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
                                 <p>All Stars 1</p>
                             </div>
                             <div className={'link'}>
-                                <img src={'rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
+                                <img src={'/rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
                                 <p>All Stars 4</p>
                             </div>
                             <div className={'link'}>
-                                <img src={'rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
+                                <img src={'/rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
                                 <p>All Stars 3</p>
                             </div>
                             <div className={'link'}>
-                                <img src={'rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
+                                <img src={'/rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
                                 <p>All Stars 4</p>
                             </div>
                         </div>
                     )}
                     <div className={'link'}>
-                        <img src={'rankedtower-web/icons/support.svg'} alt={'support icon'}/>
+                        <img src={'/rankedtower-web/icons/support.svg'} alt={'support icon'}/>
                         <p>Support</p>
                     </div>
                     <div className={'link'}>
-                        <img src={'rankedtower-web/icons/staff.svg'} alt={'shield icon'}/>
+                        <img src={'/rankedtower-web/icons/staff.svg'} alt={'shield icon'}/>
                         <p>Staffs</p>
                     </div>
                     <div className={'link'} onClick={openMapsLink}>
-                        <img src={'rankedtower-web/icons/map.svg'} alt={'pin icon'}/>
+                        <img src={'/rankedtower-web/icons/map.svg'} alt={'pin icon'}/>
                         <p>Maps</p>
                     </div>
                     <div className={'link'}>
-                        <img src={'rankedtower-web/icons/rules.svg'} alt={'list icon'}/>
+                        <img src={'/rankedtower-web/icons/rules.svg'} alt={'list icon'}/>
                         <p>Règlement</p>
                     </div>
                 </div>
@@ -153,7 +153,7 @@ const NavBar = () => {
                         <input type={'text'} placeholder={'Rechercher...'}></input>
                     </div>
                     <button type={'button'} className={'search-button'}>
-                        <img src={'rankedtower-web/icons/search.svg'} alt={'search icon'}/>
+                        <img src={'/rankedtower-web/icons/search.svg'} alt={'search icon'}/>
                     </button>
                 </div>
             </div>
