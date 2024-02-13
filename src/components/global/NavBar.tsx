aa8b1ac5145fa17519d2828fcd4544a94e4d9701
Blart Nicolas <nicolas_blart@ens.univ-artois.fr>
2024-02-13 18:17:08 +0100

    :wrench Changement du nom du repository

--- src/components/global/NavBar.tsx
+++ src/components/global/NavBar.tsx
@@ -1,11 +1,14 @@
 import React, {useEffect, useRef, useState} from 'react';
 import '../../styles/global/navbar.scss'
 
-const NavBar = () => {
+interface CurrentPageProps{
+    currentPage : string;
+}
+
+const NavBar: React.FC<CurrentPageProps> = ({ currentPage }) => {
 
     const [isMenuVisible, setMenuVisible] = useState(false);
     const [isPalmaresMenuVisible, setPalmaresMenuVisible] = useState(false);
-    const [currentPage, setCurrentPage] = useState('accueil');
     const palmaresButtonRef = useRef<HTMLDivElement>(null);
     const menuButtonRef = useRef<HTMLDivElement>(null);
     const palmaresMenuRef = useRef<HTMLDivElement>(null);
@@ -56,34 +59,35 @@ const NavBar = () => {
     };
 
     const changePage = (value: string) => {
-        setCurrentPage(value);
+        const link = "/rt-web/"+value
+        window.location.href = link;
     };
 
     return (
         <div className={'nav'}>
             <div className={'logo'}>
-                <img src={'/rankedtower-web/img/Logo.png'} alt={'Logo RT'}/>
+                <img src={'/rt-web/img/Logo.png'} alt={'Logo RT'}/>
             </div>
             <div className={'navlink'}>
                 <div className={`link ${currentPage === 'accueil' ? 'active' : ''}`}
                      onClick={() => changePage('accueil')}>
-                    <img src={'/rankedtower-web/icons/home.svg'} alt={'home icon'}/>
+                    <img src={'/rt-web/icons/home.svg'} alt={'home icon'}/>
                     <p>Accueil</p>
                 </div>
                 <div className={`link ${currentPage === 'leaderboard' ? 'active' : ''}`}
                      onClick={() => changePage('leaderboard')}>
-                    <img src={'/rankedtower-web/icons/leaderboard.svg'} alt={'medal icon'}/>
+                    <img src={'/rt-web/icons/leaderboard.svg'} alt={'medal icon'}/>
                     <p>Leaderboard</p>
                 </div>
                 <div className={`link ${currentPage === 'palmares' ? 'active' : ''}`}
                      onClick={() => {togglePalmaresMenu()} } ref={palmaresButtonRef}>
-                         < img src={'/rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
+                         < img src={'/rt-web/icons/palmares.svg'} alt={'palmares icon'}/>
                          <p>Palmarès</p>
                      {!isPalmaresMenuVisible && (
-                         <img src={'/rankedtower-web/icons/polygon.svg'} alt={'polygon icon'} className={'polygon'}/>
+                         <img src={'/rt-web/icons/polygon.svg'} alt={'polygon icon'} className={'polygon'}/>
                 )}
                 {isPalmaresMenuVisible && (
-                <img src={'/rankedtower-web/icons/polygon.svg'} alt={'polygon icon'}
+                <img src={'/rt-web/icons/polygon.svg'} alt={'polygon icon'}
                      style={{transform: 'rotate(180deg)'}}/>
                 )}
             </div>
@@ -91,69 +95,69 @@ const NavBar = () => {
                 <div className={'palmares-menu'} ref={palmaresMenuRef}>
                     <div className={`link ${currentPage === 'all stars 1' ? 'active' : ''}`}
                          onClick={() => changePage('all stars 1')}>
-                        <img src={'/rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
+                        <img src={'/rt-web/icons/palmares.svg'} alt={'palmares icon'}/>
                         <p>All Stars 1</p>
                         </div>
                     <div className={`link ${currentPage === 'all stars 2' ? 'active' : ''}`} onClick={() => changePage('all stars 2')}>
-                            <img src={'/rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
+                            <img src={'/rt-web/icons/palmares.svg'} alt={'palmares icon'}/>
                             <p>All Stars 2</p>
                         </div>
                     <div className={`link ${currentPage === 'all stars 3' ? 'active' : ''}`} onClick={() => changePage('all stars 3')}>
-                            <img src={'/rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
+                            <img src={'/rt-web/icons/palmares.svg'} alt={'palmares icon'}/>
                             <p>All Stars 3</p>
                         </div>
                     <div className={`link ${currentPage === 'all stars 4' ? 'active' : ''}`}
                          onClick={() => changePage('all stars 4')}>
-                        <img src={'/rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
+                        <img src={'/rt-web/icons/palmares.svg'} alt={'palmares icon'}/>
                         <p>All Stars 4</p>
                     </div>
                 </div>
             )}
                 <div className={`link ${currentPage === 'support' ? 'active' : ''}`}
                      onClick={() => changePage('support')}>
-                    <img src={'/rankedtower-web/icons/support.svg'} alt={'support icon'}/>
+                    <img src={'/rt-web/icons/support.svg'} alt={'support icon'}/>
                     <p>Support</p>
                 </div>
                 <div className={`link ${currentPage === 'staffs' ? 'active' : ''}`}
                      onClick={() => changePage('staffs')}>
-                    <img src={'/rankedtower-web/icons/staff.svg'} alt={'shield icon'}/>
+                    <img src={'/rt-web/icons/staff.svg'} alt={'shield icon'}/>
                     <p>Staffs</p>
                 </div>
                 <div className={'link'} onClick={openMapsLink}>
-                    <img src={'/rankedtower-web/icons/map.svg'} alt={'pin icon'}/>
+                    <img src={'/rt-web/icons/map.svg'} alt={'pin icon'}/>
                     <p>Maps</p>
                 </div>
                 <div className={`link ${currentPage === 'rules' ? 'active' : ''}`}
                      onClick={() => changePage('rules')}>
-                    <img src={'/rankedtower-web/icons/rules.svg'} alt={'list icon'}/>
+                    <img src={'/rt-web/icons/rules.svg'} alt={'list icon'}/>
                     <p>Règlement</p>
                 </div>
             </div>
             <div className={`burger ${currentPage === 'burger' ? 'active' : ''}`}
                      onClick={() => {toggleMenu()}} ref={menuButtonRef}>
-                <img src={'/rankedtower-web/icons/burger-menu.svg'} alt={'burger menu icon'}/>
+                <img src={'/rt-web/icons/burger-menu.svg'} alt={'burger menu icon'}/>
             </div>
 
             {isMenuVisible && (
                 <div className={'menu'} ref={menuRef}>
                     <div className={`link ${currentPage === 'accueil' ? 'active' : ''}`}
                          onClick={() => changePage('accueil')}>
-                        <img src={'/rankedtower-web/icons/home.svg'} alt={'home icon'}/>
+                        <img src={'/rt-web/icons/home.svg'} alt={'home icon'}/>
                         <p>Accueil</p>
                     </div>
                     <div className={`link ${currentPage === 'leaderboard' ? 'active' : ''}`}
                          onClick={() => changePage('leaderboard')}>
-                        <img src={'/rankedtower-web/icons/leaderboard.svg'} alt={'medal icon'}/>
+                        <img src={'/rt-web/icons/leaderboard.svg'} alt={'medal icon'}/>
                         <p>Leaderboard</p>
                     </div>
                     <div className={`link ${currentPage === 'palmares' ? 'active' : ''}`} onClick={togglePalmaresMenu} ref={palmaresButtonRef}>
-                        <img src={'/rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
+                        <img src={'/rt-web/icons/palmares.svg'} alt={'palmares icon'}/>
                         <p>Palmarès</p>
                         {!isPalmaresMenuVisible && (
-                            <img src={'/rankedtower-web/icons/polygon.svg'} alt={'polygon icon'} className={'polygon'}/>
+                            <img src={'/rt-web/icons/polygon.svg'} alt={'polygon icon'} className={'polygon'}/>
                         )}
                         {isPalmaresMenuVisible && (
-                            <img src={'/rankedtower-web/icons/polygon.svg'} alt={'polygon icon'} style={{transform: 'rotate(90deg)'}}/>
+                            <img src={'/rt-web/icons/polygon.svg'} alt={'polygon icon'} style={{transform: 'rotate(90deg)'}}/>
                         )}
                     </div>
                     {isPalmaresMenuVisible && (
@@ -161,42 +165,42 @@ const NavBar = () => {
                         <div className={'palmares-menu'} ref={palmaresMenuRef}>
                             <div className={`link ${currentPage === 'all stars 1' ? 'active' : ''}`}
                                  onClick={() => changePage('all stars 1')}>
-                                <img src={'/rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
+                                <img src={'/rt-web/icons/palmares.svg'} alt={'palmares icon'}/>
                                 <p>All Stars 1</p>
                             </div>
                             <div className={`link ${currentPage === 'all stars 2' ? 'active' : ''}`} onClick={() => changePage('all stars 2')}>
-                                <img src={'/rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
+                                <img src={'/rt-web/icons/palmares.svg'} alt={'palmares icon'}/>
                                 <p>All Stars 2</p>
                             </div>
                             <div className={`link ${currentPage === 'all stars 3' ? 'active' : ''}`}
                                  onClick={() => changePage('all stars 3')}>
-                                <img src={'/rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
+                                <img src={'/rt-web/icons/palmares.svg'} alt={'palmares icon'}/>
                                 <p>All Stars 3</p>
                             </div>
                             <div className={`link ${currentPage === 'all stars 4' ? 'active' : ''}`}
                                  onClick={() => changePage('all stars 4')}>
-                                <img src={'/rankedtower-web/icons/palmares.svg'} alt={'palmares icon'}/>
+                                <img src={'/rt-web/icons/palmares.svg'} alt={'palmares icon'}/>
                                 <p>All Stars 4</p>
                             </div>
                         </div>
                     )}
                     <div className={`link ${currentPage === 'support' ? 'active' : ''}`}
                          onClick={() => changePage('support')}>
-                        <img src={'/rankedtower-web/icons/support.svg'} alt={'support icon'}/>
+                        <img src={'/rt-web/icons/support.svg'} alt={'support icon'}/>
                         <p>Support</p>
                     </div>
                     <div className={`link ${currentPage === 'staffs' ? 'active' : ''}`}
                          onClick={() => changePage('staffs')}>
-                        <img src={'/rankedtower-web/icons/staff.svg'} alt={'shield icon'}/>
+                        <img src={'/rt-web/icons/staff.svg'} alt={'shield icon'}/>
                         <p>Staffs</p>
                     </div>
                     <div className={'link'} onClick={openMapsLink}>
-                        <img src={'/rankedtower-web/icons/map.svg'} alt={'pin icon'}/>
+                        <img src={'/rt-web/icons/map.svg'} alt={'pin icon'}/>
                         <p>Maps</p>
                     </div>
                     <div className={`link ${currentPage === 'rules' ? 'active' : ''}`}
                          onClick={() => changePage('rules')}>
-                        <img src={'/rankedtower-web/icons/rules.svg'} alt={'list icon'}/>
+                        <img src={'/rt-web/icons/rules.svg'} alt={'list icon'}/>
                         <p>Règlement</p>
                     </div>
                 </div>
@@ -210,7 +214,7 @@ const NavBar = () => {
                         <input type={'text'} placeholder={'Rechercher...'}></input>
                     </div>
                     <button type={'button'} className={'search-button'}>
-                        <img src={'/rankedtower-web/icons/search.svg'} alt={'search icon'}/>
+                        <img src={'/rt-web/icons/search.svg'} alt={'search icon'}/>
                     </button>
                 </div>
             </div>
